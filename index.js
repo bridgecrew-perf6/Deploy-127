5c76e91487fe36fd21e25c02afc4270c90eb602f
Prabhjyot <knowprabhjyot@gmail.com>
2022-05-27 08:13:44 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.get("/", (req, res) => {
+    res.send("Server working");
+})
+app.listen(PORT, () => {
+    console.log("Server running");
+})
